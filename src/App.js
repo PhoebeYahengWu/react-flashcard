1dc8fda02d855419cf02c1984bacbbccd4a4144b
wuyaheng <wuyaheng2016@gmail.com>
2020-08-10 15:00:22 -0400

    add sample questions

--- src/App.js
+++ src/App.js
@@ -4,9 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 3,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Flashcard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Flashcard;
