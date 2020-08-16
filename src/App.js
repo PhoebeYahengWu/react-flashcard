3ea3d6afbdf22d6a4ee244e80687d2ed1069ec0a
wuyaheng <wuyaheng2016@gmail.com>
2020-08-16 13:37:30 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
-import FlashcardList from './FlashcardList';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import FlashcardList from './FlashcardList.js';
 import './App.css';
-
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
+  useEffect(()=> {
+    axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
+    .then(res => {
+      setFlashCards(res.data.results.map((questionItem, index) => { 
+        const answer = questionItem.correct_answer;
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), answer];
+        return {
+          id: `($index)-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random() * .5),
+        }
+      })
+    )})
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }  
   return (
-    <div className="App">
-      <FlashcardList flashcards={flashcards}/>
-    </div>
+    <FlashcardList flashcards={flashcards} />
   );
 }
-
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
-    question: 'What is 2+2?',
+    question: 'What is 2 + 2?',
     answer: '4',
-    options: [
+    options: 
+    [
       '2',
       '3',
       '4',
@@ -25,26 +45,15 @@ const SAMPLE_FLASHCARDS = [
   },
   {
     id: 2,
-    question: 'What is 2+2?',
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
-  {
-    id: 3,
-    question: 'What is 2+2?',
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
+    question: 'What is 2 + 2?',
+    answer: 'Answer',
+    options: 
+    [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4'
     ]
   }
 ]
-
 export default App;
